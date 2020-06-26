6fed67169daf69eab39b8d7a2a37fadbc43a515b
jaeyeon <parkjani2@gmail.com>
2020-06-26 12:49:06 +0900

    fix expand_sections implemented cv2_save_frames_multi

--- facefinder_multi.py
+++ facefinder_multi.py
@@ -10,9 +10,10 @@ import multiprocessing
 import time
 
 TARGET_FACE_IMG_PATH = "./jeon.jpg"         #picture of my bias
-OUTPUT_VIDEO_PATH = "./only_jeon_multi10.mp4" #name of edited video
+OUTPUT_VIDEO_PATH = "./only_jeon_multi_multi.mp4" #name of edited video
 INPUT_VIDEO_PATH = "./jeon.mp4"             #name of 
 
+__ANALYZING_FRAME_DELTA = 1
 __TOLERANCE = 0.4
 __STD_PROCESS_VIDEO_WIDTH = 320             
 #fixed frame width to process. Frames' height will be resized proportionally
@@ -82,7 +83,7 @@ def child_initializer(_target_face_encoding, _resizing_scale, _analyzing_frame_d
     resizing_scale = _resizing_scale
     tolerance = _tolerance
 
-def main(target_face_img_name,input_video_file_name,output_file_name,std_process_video_width,tolerance):
+def main(target_face_img_name, input_video_file_name, output_file_name, std_process_video_width, analyzing_frame_delta, tolerance):
     #load target face(my bias)
     target_face_img = face_recognition.load_image_file(target_face_img_name)
     target_face_encoding = face_recognition.face_encodings(target_face_img)[0]
@@ -90,8 +91,9 @@ def main(target_face_img_name,input_video_file_name,output_file_name,std_process
     #load video and get video frame information
     input_video = cv2.VideoCapture(input_video_file_name)
     fps = input_video.get(cv2.CAP_PROP_FPS)
-    frame_length = input_video.get(cv2.CAP_PROP_FRAME_COUNT)
+    frame_length = input_video.get(cv2.CAP_PROP_FRAME_COUNT) 
     frame_width = input_video.get(cv2.CAP_PROP_FRAME_WIDTH)
+    input_video.release()
 
     #divide jobs 
     num_of_cpu = multiprocessing.cpu_count()
@@ -108,47 +110,46 @@ def main(target_face_img_name,input_video_file_name,output_file_name,std_process
     #reference2 : https://stackoverflow.com/questions/25825995/python-multiprocessing-only-one-process-is-running 
     matched_frame_sections = []
     with multiprocessing.get_context("spawn").Pool(num_of_cpu, initializer = child_initializer, 
-                                initargs=(target_face_encoding, resizing_scale, 1, tolerance)) as pool:
+                                initargs=(target_face_encoding, resizing_scale, analyzing_frame_delta, tolerance)) as pool:
         results = [pool.apply_async(find_target_face,(input_video_file_name, frame_range)) for frame_range in frame_ranges]
         for res in results:
             matched_frame_sections.extend(res.get())
     
-    matched_frame_sections = expand_sections(matched_frame_sections,10)
+    matched_frame_sections = expand_sections(matched_frame_sections, 0)
 
     matched_time_sections = convert_section_from_frames_to_secs(matched_frame_sections,fps)
     tmp_video_file_name = "tmp.mp4"
     tmp_audio_file_name = "tmp.mp3"
 
     #generate edited video and audo then merge them
-    cv2_save_frames(input_video, matched_frame_sections, tmp_video_file_name)
+    #cv2_save_frames(input_video_file_name, matched_frame_sections, tmp_video_file_name)
+    cv2_save_frames_multi(input_video_file_name, matched_frame_sections, tmp_video_file_name, num_of_cpu)
     trim_audio_to_audio_subclips(input_video_file_name, matched_time_sections, tmp_audio_file_name)
     ffmpeg_merge_video_audio(tmp_video_file_name, tmp_audio_file_name, output_file_name)
 
     os.remove(tmp_video_file_name)
     os.remove(tmp_audio_file_name)
 
-    input_video.release()
+    
 
 def expand_sections(frame_section_list, frame_number_to_expand = 2):
+    if frame_number_to_expand == 0 :
+        return frame_section_list
     expanded_sections = [(s-frame_number_to_expand, e + frame_number_to_expand) for s,e in frame_section_list]
-    merged_sections = []
+    merged_sections = [expanded_sections[0]]
     last_e = 0
     for section, next_section in zip(expanded_sections[:-1], expanded_sections[1:]):
-        next_s, next_e = next_section 
-        #if the last merged section overlaps next_section
-        if last_e > next_s :
-            last_s, _ = merged_sections[-1]
-            merged_sections[-1] =  (last_s, next_e)
-            last_e = next_e
-            
+        s,e = section
+        next_s, next_e = next_section
+        last_s, last_e = merged_sections[-1]
+        merged = None
+        if e >= next_s:
+            merged = (s,next_e)
+        
+        if merged:
+            merged_sections[-1] = (last_s, next_e)
         else:
-            s, e = section
-            #otherwhise (not overlaped)
-            if next_s <= e:
-                merged_sections.append((s,next_e))
-            else:
-                merged_sections.append(section)
-                merged_sections.append(next_section)
+            merged_sections.append(next_section)
 
     return merged_sections
 
@@ -161,10 +162,27 @@ def convert_section_from_frames_to_secs(section_list,fps,round_num=None):
     else:
         return [(round(s/fps,round_num), round(e/fps,round_num)) for (s,e) in section_list]
 
-def cv2_save_frames(input_video,interest_sections,output_file_name):
+def cv2_save_frames_multi(input_video_file_name,interest_sections,output_file_name,num_of_cpu):
+    section_per_cpu = len(interest_sections)//num_of_cpu
+    section_ranges = [ (section_per_cpu*cpu_idx, section_per_cpu*(cpu_idx+1)) for cpu_idx in range(num_of_cpu-1) ]
+    section_ranges.append( (section_per_cpu*(num_of_cpu-1), len(interest_sections)) )
+    sections = [ interest_sections[s:e] for s,e in section_ranges ]
+    output_names = [ "{}_subclips.mp4".format(idx) for idx in range(num_of_cpu) ]
+
+    with multiprocessing.get_context("spawn").Pool(num_of_cpu) as pool:
+        results = [pool.apply_async(cv2_save_frames, (input_video_file_name, section, output_name) ) for (section, output_name) in zip(sections,output_names)]
+        res = [res.get() for res in results]
+        
+    ffmpeg_merge_video_without_reencoding(output_names,output_file_name)
+    for name in output_names:
+        os.remove(name)    
+    
+def cv2_save_frames(input_video_file_name, interest_sections, output_file_name):
     """
     save frame by frame
     """
+    input_video = cv2.VideoCapture(input_video_file_name)
+
     fps = input_video.get(cv2.CAP_PROP_FPS)
     width = int(input_video.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(input_video.get(cv2.CAP_PROP_FRAME_HEIGHT))
@@ -179,11 +197,12 @@ def cv2_save_frames(input_video,interest_sections,output_file_name):
         elif e > frame_length : e = frame_length
 
         input_video.set(cv2.CAP_PROP_POS_FRAMES,s)
-        print("{} / {} clips saved".format(idx+1,len(interest_sections)))
+        print("PID:{}, {} / {} clips saved".format(os.getpid(), idx+1,len(interest_sections)))
         for frame_number in range(s,e):
             ret,frame = input_video.read()
             output_video.write(frame)
         idx+=1
+    input_video.release()
 
 def trim_audio_to_audio_subclips(input_video_file_name,matched_time_section,output_file_name):
     """
@@ -193,6 +212,27 @@ def trim_audio_to_audio_subclips(input_video_file_name,matched_time_section,outp
     audio_subclips = [audio.subclip(s,e) for (s,e) in matched_time_section]
     editted_audio = concatenate_audioclips(audio_subclips)
     editted_audio.write_audiofile(output_file_name)
+
+def ffmpeg_merge_video_without_reencoding(subclip_list,output_file_name):
+    #ffmpeg -f concat -safe 0 -i vidlist.txt -c copy output
+    contents = ""
+    for name in subclip_list:
+        contents += "file '{}'\n".format(name)
+
+    tmp_video_list_file = "tmp_subclips.txt"
+    with open(tmp_video_list_file,"w") as f:
+        f.write(contents)
+        
+    cmd = [get_setting("FFMPEG_BINARY"),"-y",
+            "-f", "concat",
+            "-safe", "0",
+            "-i", tmp_video_list_file,
+            "-c","copy",
+            output_file_name
+    ]
+    subprocess_call(cmd)
+    os.remove(tmp_video_list_file)
+
    
 def ffmpeg_merge_video_audio(video_file_name,audio_file_name,output_file_name):
     """
@@ -214,7 +254,7 @@ def ffmpeg_merge_video_audio(video_file_name,audio_file_name,output_file_name):
 
 if __name__ =="__main__":
     start = time.time()
-    main(TARGET_FACE_IMG_PATH, INPUT_VIDEO_PATH, OUTPUT_VIDEO_PATH, __STD_PROCESS_VIDEO_WIDTH, __TOLERANCE)
+    main(TARGET_FACE_IMG_PATH, INPUT_VIDEO_PATH, OUTPUT_VIDEO_PATH, __STD_PROCESS_VIDEO_WIDTH, __ANALYZING_FRAME_DELTA, __TOLERANCE)
     print("Processing Time: {}".format(time.time()-start))
 
 
