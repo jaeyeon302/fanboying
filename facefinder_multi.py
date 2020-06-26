679684ca81443914a9357b80530996cc34f5c4d3
jaeyeon <parkjani2@gmail.com>
2020-06-26 09:32:49 +0900

    add comments

--- facefinder_multi.py
+++ facefinder_multi.py
@@ -9,39 +9,49 @@ from moviepy.audio.AudioClip import concatenate_audioclips
 import multiprocessing
 import time
 
-TARGET_FACE_IMG_PATH = "./jeon.jpg" #"./jihyo.jpg"#"./jeon.jpg"
-OUTPUT_VIDEO_PATH = "./only_jeon_multi.mp4"#"./fancy_jihyo2.mp4"#"./test.mp4"
-INPUT_VIDEO_PATH = "./jeon.mp4"#"./videoplayback-2.mp4"#"./jeon.mp4"
+TARGET_FACE_IMG_PATH = "./jeon.jpg"         #picture of my bias
+OUTPUT_VIDEO_PATH = "./only_jeon_multi.mp4" #name of edited video
+INPUT_VIDEO_PATH = "./jeon.mp4"             #name of 
 
 __TOLERANCE = 0.4
-__STD_PROCESS_VIDEO_WIDTH = 320
+__STD_PROCESS_VIDEO_WIDTH = 320             
+#fixed frame width to process. Frames' height will be resized proportionally
 
-analyzing_frame_delta, target_face_encoding, tolerance = None,None,None
 
-Opts = nt("Opts","input_video_file_name target_face_encoding resizing_scale analyzing_frame_delta tolerance")
-        
 def find_target_face(input_video_file_name,frame_range):
+    """
+    input_video_file_name : (str) original input video file name
+    frame_range : (tuple) (start, end) - frame range to process in this funciton.
+    """
     global analyzing_frame_delta, target_face_encoding, tolerance
 
     input_video = cv2.VideoCapture(input_video_file_name)
     #video_frame_length = int(input_video.get(cv2.CAP_PROP_FRAME_COUNT))
     
+    #resize frame proportionally if frame's width > std process video width
     if resizing_scale < 1:
         resize = lambda frame: cv2.resize(frame,dsize=(0,0),fx=resizing_scale,fy=resizing_scale,interpolation=cv2.INTER_AREA)
     else:
         resize = lambda frame: frame
 
-    interest_section = []
-    saving = False
-    section_start = 0
+    interest_sections = []  #contain video section where the target face appeared
+    saving = False          #indicator whether the target face is appearing or not
+    section_start = 0       
+    
     s,e = frame_range
-    input_video.set(cv2.CAP_PROP_POS_FRAMES,s)
+    input_video.set(cv2.CAP_PROP_POS_FRAMES,s) #set the first frame 
+
+    #let's fanboying
     for frame_number in range(s,e,analyzing_frame_delta):
-        print("PID:{}, {}".format(os.getpid(),round(100*(frame_number-s)/(e-s),2)))
+        print("PID:{}, {}%".format(os.getpid(),round(100*(frame_number-s)/(e-s),2)))
+
+        #if the frames to skip to analyze are more than 0
         if analyzing_frame_delta > 1:
             input_video.set(cv2.CAP_PROP_POS_FRAMES,frame_number)
+
         ret, frame = input_video.read()
         frame = resize(frame)
+        #BGR color space(opencv used) -> RGB color space(face_recognition used)
         frame = frame[:, :, ::-1]
         
         #check frame
@@ -57,11 +67,14 @@ def find_target_face(input_video_file_name,frame_range):
         elif not matched and saving:
             print("My bias disappears! at {} frame".format(frame_number))
             saving = False
-            interest_section.append((section_start, frame_number))
+            interest_sections.append((section_start, frame_number))
 
-    return interest_section
+    return interest_sections
 
 def child_initializer(_target_face_encoding, _resizing_scale, _analyzing_frame_delta, _tolerance):
+    """
+    Initialize common variables for the multiprocessing jobs
+    """
     global target_face_encoding, resizing_scale, analyzing_frame_delta, tolerance
 
     target_face_encoding = _target_face_encoding
@@ -80,7 +93,7 @@ def main(target_face_img_name,input_video_file_name,output_file_name,std_process
     frame_length = input_video.get(cv2.CAP_PROP_FRAME_COUNT)
     frame_width = input_video.get(cv2.CAP_PROP_FRAME_WIDTH)
 
-    #divide jobs
+    #divide jobs 
     num_of_cpu = multiprocessing.cpu_count()
     frame_per_cpu = frame_length // num_of_cpu
     frame_ranges = [(frame_per_cpu*(cpu_idx),frame_per_cpu*(cpu_idx+1)) for cpu_idx in range(0,num_of_cpu-1) ]
@@ -90,7 +103,8 @@ def main(target_face_img_name,input_video_file_name,output_file_name,std_process
     #define resizing scale to process in fixed frame size
     resizing_scale = std_process_video_width/frame_width
 
-    #reference1 : https://pythonspeed.com/articles/python-multiprocessing/ #왜 내 pool은 멈출까...
+    #start multiprocessing!
+    #reference1 : https://pythonspeed.com/articles/python-multiprocessing/ # 왜 내 pool은 멈출까...
     #reference2 : https://stackoverflow.com/questions/25825995/python-multiprocessing-only-one-process-is-running 
     matched_frame_section = []
     with multiprocessing.get_context("spawn").Pool(num_of_cpu, initializer = child_initializer, 
@@ -103,18 +117,29 @@ def main(target_face_img_name,input_video_file_name,output_file_name,std_process
     tmp_video_file_name = "tmp.mp4"
     tmp_audio_file_name = "tmp.mp3"
 
+    #generate edited video and audo then merge them
     cv2_save_frames(input_video,matched_frame_section,tmp_video_file_name)
-    trim_to_audio_subclips(input_video_file_name,matched_time_section,tmp_audio_file_name)
+    trim_audio_to_audio_subclips(input_video_file_name,matched_time_section,tmp_audio_file_name)
     ffmpeg_merge_video_audio(tmp_video_file_name,tmp_audio_file_name,output_file_name)
+
+    os.remove(tmp_video_file_name)
+    os.remove(tmp_audio_file_name)
+
     input_video.release()
 
 def convert_section_from_frames_to_secs(section_list,fps,round_num=None):
+    """
+    convert sections' unit from frame number to seconds
+    """
     if round_num == None:
         return [(s/fps,e/fps) for (s,e) in section_list]
     else:
         return [(round(s/fps,round_num), round(e/fps,round_num)) for (s,e) in section_list]
 
 def cv2_save_frames(input_video,interest_sections,output_file_name):
+    """
+    save frame by frame
+    """
     fps = input_video.get(cv2.CAP_PROP_FPS)
     width = int(input_video.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(input_video.get(cv2.CAP_PROP_FRAME_HEIGHT))
@@ -129,23 +154,32 @@ def cv2_save_frames(input_video,interest_sections,output_file_name):
             output_video.write(frame)
         idx+=1
 
-def trim_to_audio_subclips(input_video_file_name,matched_time_section,output_file_name):
+def trim_audio_to_audio_subclips(input_video_file_name,matched_time_section,output_file_name):
+    """
+    save edited audio where my bias appeared in video
+    """
     audio = AudioFileClip(input_video_file_name)
     audio_subclips = [audio.subclip(s,e) for (s,e) in matched_time_section]
     editted_audio = concatenate_audioclips(audio_subclips)
     editted_audio.write_audiofile(output_file_name)
    
 def ffmpeg_merge_video_audio(video_file_name,audio_file_name,output_file_name):
-    #ffmpeg -i video.mp4 -i audio.wav -c:v copy -c:a aac output.mp4
-
+    """
+    run this command
+    $ ffmpeg -i video.mp4 -i audio.wav -c:v copy -c:a aac output.mp4
+    video.mp4 -> video_file_name
+    audio.wav -> audio_file_name
+    output.mp4 -> output_file_name
+    """
+    
     cmd = [get_setting("FFMPEG_BINARY"),"-y",
             "-i",video_file_name,
             "-i",audio_file_name,
             "-c:v", "copy",
             "-c:a", "aac",
             output_file_name]
-    print(cmd)
-    subprocess_call(cmd)
+
+    subprocess_call(cmd) #from moviepy
 
 if __name__ =="__main__":
     start = time.time()
